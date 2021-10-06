adf0e62506c4bc5b814d7de2019a8b7a4bc80b79
jlbedrillana <jlbedrillana@gmail.com>
2021-10-06 18:51:51 -0500

    first commit

--- src/main/java/com/bid/demo/DemobidApplication.java
+++ src/main/java/com/bid/demo/DemobidApplication.java
@@ -2,12 +2,19 @@ package com.bid.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemobidApplication {
+public class DemobidApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemobidApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemobidApplication.class);
 	}
 
 }
